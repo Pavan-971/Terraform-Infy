0297c675af3a65f27ca07f13c83daa4a02bfe3e5
pavan2gmail.com <pavan@gmail.com>
2020-11-26 05:28:06 +0000

    intial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws"{
+region = "us-west-2"
+
+}
+
+resource "aws_instance" "myinst"{
+ami = "ami-07a0da1997b55b23e"
+instance_type = "t2.micro"
+tags = {
+Name = "New-Key"
+}
+vpc_security_group_ids = ["sg-01087793dd7fe84d8"]
+count = 1
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "myterrbukinfy"
+    key            = "mytfstatefile/terraform.tfstate"
+    region         = "us-west-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "myterrtabinfy"
+    encrypt        = true
+  }
+}
+
+
